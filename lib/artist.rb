57bfc384c358af864954e2f5d5792812d82b8ed4
Kaushal Patel <kaushalpatel778@gmail.com>
2018-06-06 02:10:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,18 @@ class Artist
   attr_accessor :name
   def initialize(name)
     @name = name
-    @songs = []    
+    @songs = []
   end
-  
+  def add_song(song)
+    @song << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def songs
+    @songs
+  end
+  def self.song_count
+      @@song_count
+  end
 end
